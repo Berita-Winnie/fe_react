02ce40910fd08b98922ab44055a0d97af9fd7ec4
Berita-Winnie <beritawinnie@gmail.com>
2025-10-25 15:29:41 +0300

    feat(routing): set up basic app routing between pages

---
+++ burger_tailwind/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Menu from './pages/Menu'
+import Home from './pages/Home'
+import About from './pages/About'
+import Shop from './pages/Shop'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
