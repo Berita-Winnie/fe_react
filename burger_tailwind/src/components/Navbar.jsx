370d2e1143e463dbf9b19d07d62125b26a2f3886
Berita-Winnie <beritawinnie@gmail.com>
2025-10-26 05:14:51 +0300

    feat(navbar): complete layout and styling with centered links and logo alignment

--- burger_tailwind/src/components/Navbar.jsx
+++ burger_tailwind/src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import { Link, Outlet } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className="text-[rgb(252,250,216)]  flex flex-row justify-between  ">
-      <p className="font-lexend">BURGUR</p>
-      <div className="  font-sansita flex flex-col italic text-xs ">
+    <div className=" w-full text-[rgb(252,250,216)]  flex flex-row  gap-12  m-6  items-center  ">
+      <p className=" font-lexend text-3xl font-bold tracking-wider">BURGUR</p>
+      <div className=" absolute left-1/2 transform -translate-x-1/2 hidden md:flex flex-row sm:block  font-sansita gap-8 italic text-sm">
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
