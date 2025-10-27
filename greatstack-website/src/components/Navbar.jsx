72ef5c251d1930b8b6241e5468d446381dc27353
Berita-Winnie <beritawinnie@gmail.com>
2025-10-27 18:07:23 +0300

    complete navbar and header section

--- greatstack-website/src/components/Navbar.jsx
+++ greatstack-website/src/components/Navbar.jsx
@@ -1,23 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+  useEffect(() => {
+    if (showMobileMenu) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  }, [showMobileMenu])
+
   return (
     <div className="absolute top-0 left-0 w-full z-10  ">
       <div className="container mx-auto flex  justify-between items-center py-4 px-6 md:px-20 lg:px32 bg-transparent">
         <img src={assets.logo} alt="" />
         <ul className="hidden md:flex gap-7 text-white ">
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#Header"
+            className="cursor-pointer hover:text-gray-400"
+          >
             Home
           </a>
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#Header"
+            className="cursor-pointer hover:text-gray-400"
+          >
             About
           </a>
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#Header"
+            className="cursor-pointer hover:text-gray-400"
+          >
             Projects
           </a>
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#Header"
+            className="cursor-pointer hover:text-gray-400"
+          >
             Testimonials
           </a>
         </ul>
