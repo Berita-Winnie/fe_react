cec9d1c9cde57bc40afb199ebb0692c016eb1cee
Berita-Winnie <beritawinnie@gmail.com>
2025-10-27 17:59:09 +0300

    feat(navbar): enable mobile menu toggle

--- greatstack-website/src/components/Navbar.jsx
+++ greatstack-website/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
   return (
     <div className="absolute top-0 left-0 w-full z-10  ">
       <div className="container mx-auto flex  justify-between items-center py-4 px-6 md:px-20 lg:px32 bg-transparent">
@@ -23,6 +24,45 @@ const Navbar = () => {
         <button className="hidden md:block bg-white px-8 py-2 rounded-full">
           Sign Uo
         </button>
+        <img
+          onClick={() => setShowMobileMenu(true)}
+          src={assets.menu_icon}
+          className=" cursot-pointer md:hidden w-7"
+          alt=""
+        />
+      </div>
+      {/*-----------Mobile-menu------------  */}
+      <div
+        className={`md:hidden ${
+          showMobileMenu ? 'fixed w-full ' : 'h-0 w-0'
+        } right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
+      >
+        <div className="flex justify-end p-6">
+          <img
+            onClick={() => setShowMobileMenu(false)}
+            src={assets.cross_icon}
+            alt=""
+            className="w-4 cursor-pointer"
+          />
+        </div>
+
+        <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+          <a href="#Header" className="px-4 py-2 rounded-full inline-block">
+            Home
+          </a>
+          <a href="#About" className="px-4 py-2 rounded-full inline-block">
+            About
+          </a>
+          <a href="#Projects" className="px-4 py-2 rounded-full inline-block">
+            Projects
+          </a>
+          <a
+            href="#Testimonials"
+            className="px-4 py-2 rounded-full inline-block"
+          >
+            Testimonials
+          </a>
+        </ul>
       </div>
     </div>
   )
