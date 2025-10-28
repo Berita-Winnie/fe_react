3ad49b4871d2f55b50d1b5e3652bdf93397b2c19
Berita-Winnie <beritawinnie@gmail.com>
2025-10-28 12:18:05 +0300

    feat: add contact section with functional form and toast notifications

--- greatstack-website/src/App.jsx
+++ greatstack-website/src/App.jsx
@@ -1,15 +1,24 @@
 import About from './components/About'
+import Contact from './components/Contact'
 import Header from './components/Header'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={true}
+      />
       <Header />
       <About />
       <Projects />
       <Testimonials />
+      <Contact />
     </div>
   )
 }
