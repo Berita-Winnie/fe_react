cb30bdeb209c6b2ff0e8a8f033123fabf87c2e9f
Berita-Winnie <beritawinnie@gmail.com>
2025-11-06 20:59:31 +0300

    feat(auth): integrate Firebase authentication to handle user sign-in and sign-up

--- netflix_clone/src/App.jsx
+++ netflix_clone/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home/Home'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './Firebase'
 const App = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log('logged In ')
+        navigate('/')
+      } else {
+        console.log('logged out ')
+        navigate('/login')
+      }
+    })
+  }, [])
   return (
     <Routes>
       <Route path="/" element={<Home />} />
---
+++ netflix_clone/src/Firebase.js
@@ -0,0 +1,54 @@
+import { initializeApp } from 'firebase/app'
+import { createUserWithEmailAndPassword, getAuth, signOut } from 'firebase/auth'
+import { signInWithEmailAndPassword } from 'firebase/auth/cordova'
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdKhRSLTN2Hk_kFBNJKjSSO2AyWB3PWyw',
+  authDomain: 'netflix-clone-59e86.firebaseapp.com',
+  projectId: 'netflix-clone-59e86',
+  storageBucket: 'netflix-clone-59e86.firebasestorage.app',
+  messagingSenderId: '276563182851',
+  appId: '1:276563182851:web:88f20d4e02f9daa1e49536',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+//Functions
+
+//user signUp function
+const signUp = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password)
+    const user = res.user
+    await addDoc(collection(db, 'user'), {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email,
+    })
+  } catch (error) {
+    console.log(error)
+    alert(error)
+  }
+}
+
+//User logIn function
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password)
+  } catch (error) {
+    console.log(error)
+    alert(error)
+  }
+}
+
+//LogOut function
+const logOut = () => {
+  signOut(auth)
+}
+
+export { auth, db, login, signUp, logOut }
