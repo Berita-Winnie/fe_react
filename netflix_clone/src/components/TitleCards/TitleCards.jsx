4370ca1ac3be7a2b059c21cf6169020c8fc36415
Berita-Winnie <beritawinnie@gmail.com>
2025-11-06 14:38:43 +0300

    feat: integrate TMDB API and use props to display different movie sections

--- netflix_clone/src/components/TitleCards/TitleCards.jsx
+++ netflix_clone/src/components/TitleCards/TitleCards.jsx
@@ -1,24 +1,47 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([])
   const cardsRef = useRef()
-  const handleWheel = (e) => {
-    e.preventDefault()
-    cardsRef.current.scrollLeft += e.deltaY
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGM2YzE5NTU3OGE1YWMzNDM1OWQyYjc0MmEzOGMxMiIsIm5iZiI6MTc2MjQyNTAyOC41ODUsInN1YiI6IjY5MGM3OGM0NTkxODljYzQ2NWQ3MzgzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.68ffWdu6cIWZG6YGIKXo4aI6oJtTTgkDrLft4oYXORU',
+    },
   }
+
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : 'now_playing'
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err))
+
+    const handleWheel = (e) => {
+      e.preventDefault()
+      cardsRef.current.scrollLeft += e.deltaY
+    }
     cardsRef.current.addEventListener('wheel', handleWheel)
   }, [])
   return (
     <div className="title-cards">
       <h2>{title ? title : 'Popular on Netflix'}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => (
+        {apiData.map((card, index) => (
           <div key={index} className="card ">
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img
+              src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+              alt=""
+            />
+            <p>{card.original_title}</p>
           </div>
         ))}
       </div>
