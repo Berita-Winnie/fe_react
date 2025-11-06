41f9e65cdbbc3547ef8e4fac875f6ec889567f2d
Berita-Winnie <beritawinnie@gmail.com>
2025-11-06 13:20:39 +0300

    style: finish UI for login and signup forms

--- netflix_clone/src/App.jsx
+++ netflix_clone/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './pages/Home/Home'
-
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login/Login'
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   )
 }
 
