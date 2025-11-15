812cb36be2ea5250d38e3f3192caff9119c246fc
Berita-Winnie <beritawinnie@gmail.com>
2025-11-15 17:43:15 +0300

    complete hero- section fixing gradient

--- agency_ai/src/App.jsx
+++ agency_ai/src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
 const App = () => {
   const [theme, setTheme] = useState(
     localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light'
   )
   return (
-    <div>
+    <div className="dark:bg-black relative">
       <NavBar theme={theme} setTheme={setTheme} />
+      <Hero />
     </div>
   )
 }
