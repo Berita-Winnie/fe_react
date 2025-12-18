b52c137b208643b1376afc8921c4ca2184749282
Berita-Winnie <beritawinnie@gmail.com>
2025-12-18 11:52:21 +0300

    Complete DisplayHome layout for album categories and hits

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import { albumsData } from '../assets/assets'
+import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
+import SongItem from './SongItem'
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
-      <div className="my-5 font-bold text-2xl "></div>
+      <div className="mb-4 ">
+        <h1 className="my-5 font-bold text-2xl ">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item) => (
+            <AlbumItem
+              key={item.id}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="mb-4 ">
+        <h1 className="my-5 font-bold text-2xl ">Todays Biggest Hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item) => (
+            <SongItem
+              key={item.id}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
     </>
   )
 }
--- spotify-clone/src/components/Sidebar.jsx
+++ spotify-clone/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     <div className="w-[25%] h-full p-2 flex-col gap-2  text-white hidden lg:flex">
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
         <div className="flex items-center gap-3 pl-8 cursor-pointer">
-          <img src={assets.home_icon} alt="" />
+          <img className="w-6" src={assets.home_icon} alt="" />
           <p className="font-bold ">Home</p>
         </div>
         <div className="flex items-center gap-3 pl-8 cursor-pointer">
@@ -27,14 +27,16 @@ const Sidebar = () => {
         </div>
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
           <h1>Create your first playlist</h1>
-          <p className=" font-light">It's easy we will help you</p>
+          <p className=" font-normal text-sm">It's easy we will help you</p>
           <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 ">
             Create Playlist
           </button>
         </div>
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
           <h1>Let's find some podcasts to follow</h1>
-          <p className=" font-light">We'll keep you updated on new episodes</p>
+          <p className=" font-normal text-sm">
+            We'll keep you updated on new episodes
+          </p>
           <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 ">
             Browse podcasts
           </button>
